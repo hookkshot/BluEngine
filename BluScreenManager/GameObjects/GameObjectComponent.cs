191a53be15885176dbcd741ae5e10dc34386c64c
Hookkshot <stachkeirron@gmail.com>
2013-07-30 23:07:28 +0930

    GameComponent Update
    Added generic sprite NOTE(need to add rotation to GameObjects)
    add initialize to components also.

--- BluScreenManager/GameObjects/GameObjectComponent.cs
+++ BluScreenManager/GameObjects/GameObjectComponent.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 namespace BluEngine.GameObjects
 {
@@ -12,10 +13,29 @@ namespace BluEngine.GameObjects
     {
         #region Fields
 
+        protected GameObject connectedGameObject = null;
+
+        #endregion
+
+        #region Properties
+
+        public GameObject ConnectedGameObject
+        {
+            get { return connectedGameObject; }
+            set { connectedGameObject = value; }
+        }
 
         #endregion
 
         #region Initialize
+
+        /// <summary>
+        /// Sets up the component if it needs extra work after being loaded.
+        /// </summary>
+        /// <param name="path">The path that all your game content is placed</param>
+        public virtual void Initialize(ContentManager content, string path)
+        {
+        }
 
         #endregion
 
---
+++ BluScreenManager/GameObjects/Sprite.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace BluEngine.GameObjects
+{
+    public class Sprite : GameObjectComponent
+    {
+        #region Fields
+
+        [XmlIgnore()]
+        protected Texture2D sourceImage;
+        protected string sourceImageName = "";
+
+        protected float layer = 0.5f;
+
+        #endregion
+
+        #region Properties
+
+        public Texture2D SourceImage
+        {
+            get { return sourceImage; }
+            set { sourceImage = value; }
+        }
+
+        public string SourceImageName
+        {
+            get { return sourceImageName; }
+            set { sourceImageName = value; }
+        }
+
+        public float Layer
+        {
+            get { return layer; }
+            set { layer = value; }
+        }
+
+        #endregion
+
+        #region Initialize
+
+        public override void Initialize(ContentManager content, string path)
+        {
+            sourceImage = content.Load<Texture2D>(Path.Combine(path, sourceImageName));
+        }
+
+        #endregion
+
+        #region Update
+
+        #endregion
+
+        #region Draw
+
+        public override void Draw(SpriteBatch spriteBatch, Vector2 screenOffset)
+        {
+            spriteBatch.Draw(sourceImage, ConnectedGameObject.Position, sourceImage.Bounds, Color.White, 0.0f, new Vector2(sourceImage.Width/2,sourceImage.Height/2),1.0f, SpriteEffects.None, layer);
+        }
+
+        #endregion
+    }
+}
