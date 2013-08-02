846ee1af74a47056446e3edfe4e8e2772a25f872
Mark Gillard <marzer_@hotmail.com>
2013-08-02 13:19:37 +0930

    added default xtor

--- BluScreenManager/Engine/HierarchicalDrawable.cs
+++ BluScreenManager/Engine/HierarchicalDrawable.cs
@@ -9,6 +9,17 @@ namespace BluEngine.Engine
     public abstract class HierarchicalDrawable : HierarchicalObject
     {
         /// <summary>
+        /// Create a new HierarchicalDrawable with a given parent.
+        /// </summary>
+        /// <param name="parent">The HierarchicalDrawable's parent.</param>
+        public HierarchicalDrawable(HierarchicalDrawable parent) : base(parent) { }
+
+        /// <summary>
+        /// Create a new HierarchicalDrawable.
+        /// </summary>
+        public HierarchicalDrawable() : this(null) { }
+        
+        /// <summary>
         /// Draws itself then all children. Call this once on your base object.
         /// </summary>
         /// <param name="spriteBatch">The SpriteBatch object used for drawing.</param>
