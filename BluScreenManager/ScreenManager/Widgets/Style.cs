e3c073ec777f0908fc49ae0e0fc46752811e8ade
Mark Gillard <marzer_@hotmail.com>
2013-08-02 14:37:40 +0930

    base-level Widget Style

---
+++ BluScreenManager/ScreenManager/Widgets/Style.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using BluEngine.Engine;
+
+namespace BluEngine.ScreenManager.Widgets
+{
+    public class Style : HierarchicalObject
+    {
+        new public Style Parent
+        {
+            get { return base.Parent == null ? null : base.Parent as Style; }
+            set { base.Parent = value; }
+        }
+
+        private Texture2D fill = null;
+        public Texture2D Fill
+        {
+            get { return fill == null ? (Parent == null ? null : Parent.Fill) : fill; }
+            set { fill = value; }
+        }
+
+        public Style(Style parent) : base(parent) {}
+        public Style() : base() { }
+    }
+}
