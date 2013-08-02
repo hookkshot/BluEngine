7e02e027a82c6eabb81cd95b9b6158bfd315409e
Mark Gillard <marzer_@hotmail.com>
2013-08-02 12:45:56 +0930

    added HierarchicalObject base classes

---
+++ BluScreenManager/Engine/HierarchicalDrawable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BluEngine.Engine
+{
+    public abstract class HierarchicalDrawable : HierarchicalObject
+    {
+        /// <summary>
+        /// Draws itself then all children. Call this once on your base object.
+        /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch object used for drawing.</param>
+        public void DrawAll(SpriteBatch spriteBatch)
+        {
+            Draw(spriteBatch);
+            foreach (HierarchicalObject obj in Children)
+            {
+                HierarchicalDrawable drawable = obj as HierarchicalDrawable;
+                if (drawable != null)
+                    drawable.DrawAll(spriteBatch);
+            }
+        }
+
+        /// <summary>
+        /// Draws itself. Override this to implement your drawing functionality for this object.
+        /// You do not need to call this directly; it is called hierarchically by the base object via DrawAll().
+        /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch object used for drawing.</param>
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+}
---
+++ BluScreenManager/Engine/HierarchicalObject.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BluEngine.Engine
+{
+    public class HierarchicalObject
+    {
+        /// <summary>
+        /// The list of children belonging to this object. Do not modify this list directly!
+        /// </summary>
+        protected List<HierarchicalObject> Children
+        {
+            get { return children; }
+        }
+        private List<HierarchicalObject> children = new List<HierarchicalObject>();
+        
+        /// <summary>
+        /// This object's parent in the hierarchy.
+        /// Will throw an InvalidOperationException if an attempt to create a hierarchical loop is made.
+        /// </summary>
+        public HierarchicalObject Parent
+        {
+            get { return parent; }
+            set
+            {
+                if (parent == value)
+                    return;
+
+                if (value != null && value.isAncestor(this))
+                    throw new InvalidOperationException("You cannot set a HierarchicalObject's parent to one of it's children!");
+
+                if (parent != null)
+                    parent.children.Remove(this);
+                parent = value;
+                if (parent != null)
+                    parent.children.Add(this);
+            }
+        }
+        private HierarchicalObject parent;
+
+        /// <summary>
+        /// Create a HierarchicalObject instance.
+        /// </summary>
+        /// <param name="parent">The HierarchicalObject's parent.</param>
+        public HierarchicalObject(HierarchicalObject parent)
+        {
+            Parent = parent;
+        }
+
+        /// <summary>
+        /// Create a HierarchicalObject instance.
+        /// </summary>
+        public HierarchicalObject() : this(null) { }
+
+        /// <summary>
+        /// Check if a HierarchicalObject is an ancestor of this one.
+        /// </summary>
+        /// <param name="potentialAncestor">The value to check.</param>
+        /// <returns>True if potentialAncestor was in the parent hierarchy of the current HierarchicalObject.</returns>
+        public bool isAncestor(HierarchicalObject potentialAncestor)
+        {
+            if (potentialAncestor == null)
+                return false;
+
+            if (potentialAncestor == this)
+                return true;
+
+            return parent == null ? false : parent.isAncestor(potentialAncestor);
+        }
+    }
+}
