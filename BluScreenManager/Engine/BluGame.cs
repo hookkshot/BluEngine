22ebada80a93a84e4198c5f45ac201c851a10e52
Mark Gillard <marzer_@hotmail.com>
2013-08-03 17:24:17 +0930

    added BluGame subclass of XNA Game

---
+++ BluScreenManager/Engine/BluGame.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using BluEngine;
+using BluEngine.ScreenManager;
+using BluEngine.ScreenManager.Widgets;
+using BluEngine.TweenAccessors;
+using AurelienRibon.TweenEngine;
+
+
+namespace BluEngine.Engine
+{
+	public class BluGame : Game
+	{
+        private GraphicsDeviceManager graphics;
+
+        /// <summary>
+        /// ScreenManager instance in use by this BluGame.
+        /// </summary>
+        public BluEngine.ScreenManager.ScreenManager ScreenManager
+        {
+            get { return screenManager; }
+        }
+        private BluEngine.ScreenManager.ScreenManager screenManager;
+
+        public BluGame() : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.PreferredBackBufferWidth = 1280;
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            this.IsMouseVisible = true;
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            RegisterTweenAccessors();
+            screenManager = new BluEngine.ScreenManager.ScreenManager(this, "Fonts\\smallfont");
+            screenManager.Initialize();
+        }
+
+        protected virtual void RegisterTweenAccessors()
+        {
+            Tween.RegisterAccessor(typeof(Widget), new WidgetAccessor());
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            screenManager.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+            screenManager.Draw(gameTime);
+        }
+	}
+}
