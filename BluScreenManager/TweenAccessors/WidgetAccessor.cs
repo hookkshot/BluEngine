b37dc8655fe94c873738b733248d0b3c632bb24f
Mark Gillard <marzer_@hotmail.com>
2013-08-05 08:28:40 +0930

    added width and height tweens

--- BluScreenManager/TweenAccessors/WidgetAccessor.cs
+++ BluScreenManager/TweenAccessors/WidgetAccessor.cs
@@ -11,7 +11,10 @@ namespace BluEngine.TweenAccessors
     {
         public const int TWEEN_HORIZONTAL = 1;
         public const int TWEEN_VERTICAL = 2;
-        public const int TWEEN_HORIZ_VERT = 3;
+        public const int TWEEN_POSITION = 3;
+        public const int TWEEN_WIDTH = 4;
+        public const int TWEEN_HEIGHT = 5;
+        public const int TWEEN_SIZE = 6;
 
         public override int GetValues(object target, int tweenType, float[] returnValues)
         {
@@ -26,9 +29,22 @@ namespace BluEngine.TweenAccessors
                     returnValues[0] = widget.Top;
                     return 1;
 
-                case TWEEN_HORIZ_VERT:
+                case TWEEN_POSITION:
                     returnValues[0] = widget.Left;
                     returnValues[1] = widget.Top;
+                    return 2;
+
+                case TWEEN_WIDTH:
+                    returnValues[0] = widget.Width;
+                    return 1;
+
+                case TWEEN_HEIGHT:
+                    returnValues[0] = widget.Height;
+                    return 1;
+
+                case TWEEN_SIZE:
+                    returnValues[0] = widget.Width;
+                    returnValues[1] = widget.Height;
                     return 2;
             }
             return 0;
@@ -47,9 +63,22 @@ namespace BluEngine.TweenAccessors
                     widget.Top = newValues[0];
                     break;
 
-                case TWEEN_HORIZ_VERT:
+                case TWEEN_POSITION:
                     widget.Left = newValues[0];
                     widget.Top = newValues[1];
+                    break;
+
+                case TWEEN_WIDTH:
+                    widget.Width = newValues[0];
+                    break;
+
+                case TWEEN_HEIGHT:
+                    widget.Height = newValues[0];
+                    break;
+
+                case TWEEN_SIZE:
+                    widget.Width = newValues[0];
+                    widget.Height = newValues[1];
                     break;
 
             }
