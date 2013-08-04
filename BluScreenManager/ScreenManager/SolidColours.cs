a4b3c5116127db413830f8f13497ab061a068752
Mark Gillard <marzer_@hotmail.com>
2013-08-05 08:29:15 +0930

    added percentage-blacks

--- BluScreenManager/ScreenManager/SolidColours.cs
+++ BluScreenManager/ScreenManager/SolidColours.cs
@@ -12,7 +12,8 @@ namespace BluEngine.ScreenManager
     /// </summary>
     public abstract class SolidColours
     {
-        private static Texture2D red, green, blue, cyan, magenta, yellow, black, white, darkgray, gray, lightgray, dimgray, darkdarkgray;
+        private static Texture2D red, green, blue, cyan, magenta, yellow, black, white;
+        private static Texture2D black95, black98, black90, black85, black80, black70, black60, black50;
 
         private static Texture2D InitializeColour(Color color, out Texture2D tex)
         {
@@ -27,12 +28,51 @@ namespace BluEngine.ScreenManager
         public static Texture2D Cyan { get { return cyan ?? InitializeColour(Color.Cyan, out cyan); } }
         public static Texture2D Yellow { get { return yellow ?? InitializeColour(Color.Yellow, out yellow); } }
         public static Texture2D Magenta { get { return magenta ?? InitializeColour(Color.Magenta, out magenta); } }
-        public static Texture2D Black { get { return black ?? InitializeColour(Color.Black, out black); } }
         public static Texture2D White { get { return white ?? InitializeColour(Color.White, out white); } }
-        public static Texture2D DarkGray { get { return darkgray ?? InitializeColour(Color.DarkGray, out darkgray); } }
-        public static Texture2D Gray { get { return gray ?? InitializeColour(Color.Gray, out gray); } }
-        public static Texture2D LightGray { get { return lightgray ?? InitializeColour(Color.LightGray, out lightgray); } }
-        public static Texture2D DimGray { get { return dimgray ?? InitializeColour(Color.DimGray, out dimgray); } }
-        public static Texture2D DarkDarkGray { get { return darkdarkgray ?? InitializeColour(Color.FromNonPremultiplied(20,20,20,255), out darkdarkgray); } }
+
+        /// <summary>
+        /// 100% Black.
+        /// </summary>
+        public static Texture2D Black { get { return black ?? InitializeColour(Color.Black, out black); } }
+
+        /// <summary>
+        /// 98% Black.
+        /// </summary>
+        public static Texture2D Black98 { get { return black98 ?? InitializeColour(Color.FromNonPremultiplied(5, 5, 5, 255), out black98); } }
+
+        /// <summary>
+        /// 95% Black.
+        /// </summary>
+        public static Texture2D Black95 { get { return black95 ?? InitializeColour(Color.FromNonPremultiplied(13,13, 13, 255), out black95); } }
+
+        /// <summary>
+        /// 90% Black.
+        /// </summary>
+        public static Texture2D Black90 { get { return black90 ?? InitializeColour(Color.FromNonPremultiplied(25, 25, 25, 255), out black90); } }
+
+        /// <summary>
+        /// 85% Black.
+        /// </summary>
+        public static Texture2D Black85 { get { return black85 ?? InitializeColour(Color.FromNonPremultiplied(38, 38, 38, 255), out black85); } }
+
+        /// <summary>
+        /// 80% Black.
+        /// </summary>
+        public static Texture2D Black80 { get { return black80 ?? InitializeColour(Color.FromNonPremultiplied(51, 51, 51, 255), out black80); } }
+
+        /// <summary>
+        /// 70% Black.
+        /// </summary>
+        public static Texture2D Black70 { get { return black70 ?? InitializeColour(Color.FromNonPremultiplied(77, 77, 77, 255), out black70); } }
+
+        /// <summary>
+        /// 60% Black.
+        /// </summary>
+        public static Texture2D Black60 { get { return black60 ?? InitializeColour(Color.FromNonPremultiplied(102, 102, 102, 255), out black60); } }
+
+        /// <summary>
+        /// 50% Black.
+        /// </summary>
+        public static Texture2D Black50 { get { return black50 ?? InitializeColour(Color.FromNonPremultiplied(128, 128, 128, 255), out black50); } }
     }
 }
