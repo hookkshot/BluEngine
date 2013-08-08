85ea0118043cdbe717d3e0dcc9c866682e7f805f
Hookkshot <stachkeirron@gmail.com>
2013-08-09 01:11:35 +0930

    Added View Screen class

---
+++ BluScreenManager/Engine/GameObjects/ViewScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BluEngine.Engine.GameObjects
+{
+    public class ViewScreen
+    {
+        private int width = 0;
+        private int height = 0;
+
+        public ViewScreen(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Width
+        {
+            get { return width; }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+        }
+    }
+}
