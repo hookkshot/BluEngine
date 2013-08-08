776311a898ff10b687ae19286a8522b627863595
Mark Gillard <marzer_@hotmail.com>
2013-08-09 02:52:34 +0930

    new Properties tweenable in Widget

--- BluScreenManager/TweenAccessors/WidgetAccessor.cs
+++ BluScreenManager/TweenAccessors/WidgetAccessor.cs
@@ -4,20 +4,66 @@ using System.Linq;
 using System.Text;
 using AurelienRibon.TweenEngine;
 using BluEngine.ScreenManager.Widgets;
+using Microsoft.Xna.Framework;
 
 namespace BluEngine.TweenAccessors
 {
     public class WidgetAccessor : TweenAccessor
     {
+        /// <summary>
+        /// Tween the widget's Left value.
+        /// </summary>
         public const int TWEEN_HORIZONTAL = 1;
+
+        /// <summary>
+        /// Tween the widget's Top value.
+        /// </summary>
         public const int TWEEN_VERTICAL = 2;
+
+        /// <summary>
+        /// Tween the widget's Left,Top values.
+        /// </summary>
         public const int TWEEN_POSITION = 3;
+
+        /// <summary>
+        /// Tween the widget's Width value.
+        /// </summary>
         public const int TWEEN_WIDTH = 4;
+
+        /// <summary>
+        /// Tween the widget's Height value.
+        /// </summary>
         public const int TWEEN_HEIGHT = 5;
+
+        /// <summary>
+        /// Tween the widget's Width, Height values.
+        /// </summary>
         public const int TWEEN_SIZE = 6;
+
+        /// <summary>
+        /// Tween the widget's Top, Width, Height values.
+        /// </summary>
         public const int TWEEN_TOP_SIZE = 7;
+
+        /// <summary>
+        /// Tween the widget's TintStrength value.
+        /// </summary>
         public const int TWEEN_TINT_STRENGTH = 8;
+
+        /// <summary>
+        /// Tween the widget's Top, Width, Height, TintStrength values.
+        /// </summary>
         public const int TWEEN_TOP_SIZE_TINT_STRENGTH = 9;
+
+        /// <summary>
+        /// Tween the widget's Tint value (R, G, B as floats between 0.0f and 1.0f).
+        /// </summary>
+        public const int TWEEN_TINT = 10;
+
+        /// <summary>
+        /// Tween the widget's Alpha value.
+        /// </summary>
+        public const int TWEEN_ALPHA = 11;
 
         public override int GetValues(object target, int tweenType, float[] returnValues)
         {
@@ -66,6 +112,17 @@ namespace BluEngine.TweenAccessors
                     returnValues[2] = widget.Height;
                     returnValues[3] = widget.TintStrength;
                     return 4;
+
+                case TWEEN_TINT:
+                    Color tint = widget.Tint;
+                    returnValues[0] = (float)tint.R / 255.0f;
+                    returnValues[1] = (float)tint.G / 255.0f;
+                    returnValues[2] = (float)tint.B / 255.0f;
+                    return 3;
+
+                case TWEEN_ALPHA:
+                    returnValues[0] = widget.Alpha;
+                    return 1;
             }
             return 0;
         }
@@ -116,6 +173,14 @@ namespace BluEngine.TweenAccessors
                     widget.Width = newValues[1];
                     widget.Height = newValues[2];
                     widget.TintStrength = newValues[3];
+                    break;
+
+                case TWEEN_TINT:
+                    widget.Tint = new Color(newValues[0], newValues[1], newValues[2]);
+                    break;
+
+                case TWEEN_ALPHA:
+                    widget.Alpha = newValues[0];
                     break;
             }
         }
