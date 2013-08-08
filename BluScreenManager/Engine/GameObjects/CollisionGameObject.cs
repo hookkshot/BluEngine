59e6ba1688e5a60005dcc3372f0c41f39939ef64
Mark Gillard <marzer_@hotmail.com>
2013-08-09 04:19:15 +0930

    pulled CollisionGameObject up into base engine

---
+++ BluScreenManager/Engine/GameObjects/CollisionGameObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BluEngine.Engine.GameObjects
+{
+    public class CollisionGameObject : GameObject
+    {
+        protected bool active = true;
+
+        public CollisionBoxComponent TransformBox
+        {
+            get { return Transform as CollisionBoxComponent; }
+        }
+
+        public CollisionGameObject()
+            : base()
+        {
+            Transform = new CollisionBoxComponent();
+        }
+    }
+}
