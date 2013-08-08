89a7279d0b7440e59dd57723529168b9bc3ae5b8
Mark Gillard <marzer_@hotmail.com>
2013-08-09 03:39:42 +0930

    BluGameInitSettings class

--- BluScreenManager/Engine/BluGame.cs
+++ BluScreenManager/Engine/BluGame.cs
@@ -4,6 +4,7 @@ using BluEngine.ScreenManager.Screens;
 using BluEngine.ScreenManager.Widgets;
 using BluEngine.TweenAccessors;
 using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 
 
 namespace BluEngine.Engine
@@ -11,7 +12,7 @@ namespace BluEngine.Engine
 	public class BluGame : Game
 	{
         private GraphicsDeviceManager graphics;
-        private Type firstScreen = null;
+        private BluGameInitSettings initSettings = null;
 
         /// <summary>
         /// ScreenManager instance in use by this BluGame.
@@ -22,38 +23,45 @@ namespace BluEngine.Engine
         }
         private BluEngine.ScreenManager.ScreenManager screenManager;
 
-        public BluGame(Type firstScreenType) : base()
+        /// <summary>
+        /// Create a new instance of BluGame.
+        /// </summary>
+        /// <param name="settings">A settings blob to pass. Passing null will assume defaults.</param>
+        public BluGame(BluGameInitSettings settings)
+            : base()
         {
+            initSettings = settings ?? new BluGameInitSettings();
+
             graphics = new GraphicsDeviceManager(this);
-            graphics.PreferredBackBufferHeight = 720;
-            graphics.PreferredBackBufferWidth = 1280;
-            Content.RootDirectory = "Content";
-            firstScreen = firstScreenType;
+            graphics.PreferredBackBufferWidth = initSettings.ScreenWidth;
+            graphics.PreferredBackBufferHeight = initSettings.ScreenHeight;
+            graphics.IsFullScreen = initSettings.IsFullscreen;
+            Content.RootDirectory = initSettings.ContentRoot;
         }
+
+        /// <summary>
+        /// Create a new instance of BluGame with default settings.
+        /// </summary>
         public BluGame() : this(null) { }
 
         protected override void Initialize()
         {
             base.Initialize();
-            IsMouseVisible = true;
+            IsMouseVisible = initSettings.IsMouseVisible;
         }
 
         protected override void LoadContent()
         {
             base.LoadContent();
-            RegisterTweenAccessors();
-            screenManager = new BluEngine.ScreenManager.ScreenManager(this, "Fonts\\smallfont");
+            Tween.SetCombinedAttributesLimit(initSettings.TweenAttributeLimit);
+            if (initSettings.TweenAccessors != null)
+            {
+                foreach (KeyValuePair<Type, TweenAccessor> kvp in initSettings.TweenAccessors)
+                    Tween.RegisterAccessor(kvp.Key, kvp.Value);
+            }
+            screenManager = new BluEngine.ScreenManager.ScreenManager(this, initSettings.DefaultFont);
             screenManager.Initialize();
-            screenManager.AddScreen(firstScreen); //won't do anything if firstScreen is null or invalid :)
-        }
-
-        /// <summary>
-        /// Add TweenAccessors you'd like to have available from game initialization in this function. It's mainly just to have a neat, logical grouping for them (as opposed to just having them listed in LoadContent()).
-        /// </summary>
-        private void RegisterTweenAccessors()
-        {
-            Tween.SetCombinedAttributesLimit(5);
-            Tween.RegisterAccessor(typeof(Widget), new WidgetAccessor());
+            screenManager.AddScreen(initSettings.FirstScreenType); //won't do anything if firstScreen is null or invalid :)
         }
 
         protected override void Update(GameTime gameTime)
@@ -64,7 +72,7 @@ namespace BluEngine.Engine
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(initSettings.GraphicsClearColor);
             base.Draw(gameTime);
             screenManager.Draw(gameTime);
         }
---
+++ BluScreenManager/Engine/BluGameInitSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using BluEngine.TweenAccessors;
+using BluEngine.ScreenManager.Widgets;
+using AurelienRibon.TweenEngine;
+
+namespace BluEngine.Engine
+{
+    public class BluGameInitSettings
+    {
+        public int ScreenWidth = 1280;
+        public int ScreenHeight = 720;
+        public String ContentRoot = "Content";
+        public Type FirstScreenType = null;
+        public bool IsMouseVisible = true;
+        public bool IsFullscreen = false;
+        public bool IsBorderless = false;
+        public String DefaultFont = "Fonts\\smallfont";
+        public int TweenAttributeLimit = 5;
+        public Dictionary<Type, TweenAccessor> TweenAccessors = new Dictionary<Type, TweenAccessor>();
+        public Color GraphicsClearColor = Color.CornflowerBlue;
+
+
+        public BluGameInitSettings(Type firstScreenType)
+        {
+            FirstScreenType = firstScreenType;
+            TweenAccessors.Add(typeof(Widget), new WidgetAccessor());
+        }
+        public BluGameInitSettings() : this(null) { }
+
+    }
+}
