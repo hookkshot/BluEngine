bf4fcc3156b479368e60ea8bf82093471f9b3ecc
Mark Gillard <marzer_@hotmail.com>
2013-08-13 14:48:05 +0930

    docs, sealed WidgetScreenTransitionEventCallback

--- BluScreenManager/ScreenManager/Widgets/WidgetScreenTransitionCallback.cs
+++ BluScreenManager/ScreenManager/Widgets/WidgetScreenTransitionCallback.cs
@@ -7,24 +7,40 @@ using BluEngine.ScreenManager.Screens;
 
 namespace BluEngine.ScreenManager.Widgets
 {
-    
-    
+    /// <summary>
+    /// A general subclass of TweenCallback from which to subclass your own custom WidgetScreen-related callback handlers.
+    /// </summary>
+    /// <typeparam name="T">The Type of your WidgetScreen.</typeparam>
     public abstract class WidgetScreenTransitionCallback<T> : TweenCallback
         where T : WidgetScreen
     {
         protected T screen;
+
+        /// <summary>
+        /// Create a new instance of WidgetScreenTransitionCallback.
+        /// </summary>
+        /// <param name="screen">The screen this belongs to.</param>
         public WidgetScreenTransitionCallback(T screen)
         {
             this.screen = screen;
         }
     }
 
-    public class WidgetScreenTransitionEventCallback<T> : WidgetScreenTransitionCallback<T>
+    /// <summary>
+    /// A specialized subclass of WidgetScreenTransitionCallback that will take a paramaterless void function as a parameter, which will be executed when the callback is fired.
+    /// </summary>
+    /// <typeparam name="T">The Type of your WidgetScreen.</typeparam>
+    public sealed class WidgetScreenTransitionEventCallback<T> : WidgetScreenTransitionCallback<T>
         where T : WidgetScreen
     {
         public delegate void GenericEventHandler();
         private event GenericEventHandler onFinishedEvent;
 
+        /// <summary>
+        /// Create a new instance of WidgetScreenTransitionEventCallback.
+        /// </summary>
+        /// <param name="screen">The screen this belongs to.</param>
+        /// <param name="finishedEvent">The function to call when the callback is fired.</param>
         public WidgetScreenTransitionEventCallback(T screen, GenericEventHandler finishedEvent)
             : base(screen)
         {
