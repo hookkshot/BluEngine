85f3e341cbae1d3fbfb4dc76090174bb4861cfba
Mark Gillard <marzer_@hotmail.com>
2013-08-15 16:11:32 +0930

    added debugmode stuff

---
+++ BluScreenManager/ScreenManager/Styles/CSS/BluCSSParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Marzersoft.CSS;
+using BluEngine.ScreenManager.Screens;
+
+namespace BluEngine.ScreenManager.Styles.CSS
+{
+	public class BluCSSParser : CSSParser
+	{
+        public GameScreen ActiveScreen
+        {
+            get { return screen; }
+            set { screen = value; }
+        }
+        private GameScreen screen = null;
+
+        public bool DebuggerMode
+        {
+            get { return debuggerMode; }
+            set { debuggerMode = value; }
+        }
+        private bool debuggerMode = false;
+
+        public BluCSSParser(List<CSSPropertyInterpreter> interpreters, bool debuggerMode) : base(interpreters)
+        {
+            this.debuggerMode = debuggerMode;
+        }
+        public BluCSSParser(List<CSSPropertyInterpreter> interpreters) : this(interpreters,false) { }
+	}
+}
