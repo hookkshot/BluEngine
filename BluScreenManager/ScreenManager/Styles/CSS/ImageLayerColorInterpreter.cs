188617e5f3068333fc2eaaa540826ef6a6bfdf7b
Mark Gillard <marzer_@hotmail.com>
2013-08-15 16:11:22 +0930

    split functionality between input types

---
+++ BluScreenManager/ScreenManager/Styles/CSS/ImageLayerColorInterpreter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Marzersoft.CSS;
+using Microsoft.Xna.Framework.Graphics;
+using System.Text.RegularExpressions;
+using Microsoft.Xna.Framework;
+
+namespace BluEngine.ScreenManager.Styles.CSS
+{
+    public class ImageLayerColorInterpreter : CSSPropertyInterpreter
+    {
+        public ImageLayerColorInterpreter()
+            : base(
+            "layer-[0-" + (StyleSheet.STYLE_LAYERS - 1) + "]",
+            //add more ImageLayer properties here as the needs for them in CSS grows
+            CSSConstants.COLOUR
+            ) { }
+
+        protected override ICSSProperty TranslateValue(Match nameMatch, Match valueMatch)
+        {
+            BluCSSParser bluParser = (Parser as BluCSSParser);
+            
+            CSSColor cssColor = new CSSColorProperty("", valueMatch).Value;
+            ImageLayer layer = new ImageLayer(bluParser.DebuggerMode ? null : SolidColours.TexFromColor(new Color((int)cssColor.R, (int)cssColor.G, (int)cssColor.B, (int)(cssColor.A * 255.0f))));
+            layer.Name = nameMatch.Value;
+            return layer;
+        }
+
+    }
+}
---
+++ BluScreenManager/ScreenManager/Styles/CSS/ImageLayerURLInterpreter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Marzersoft.CSS;
+using System.Text.RegularExpressions;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using BluEngine.ScreenManager.Screens;
+
+namespace BluEngine.ScreenManager.Styles.CSS
+{
+    public class ImageLayerURLInterpreter : CSSPropertyInterpreter
+    {
+        public ImageLayerURLInterpreter()
+            : base(
+            "layer-[0-"+(StyleSheet.STYLE_LAYERS-1)+"]",
+            //add more ImageLayer properties here as the needs for them in CSS grows
+            CSSConstants.URI
+            ) { }
+
+        protected override ICSSProperty TranslateValue(Match nameMatch, Match valueMatch)
+        {
+            BluCSSParser bluParser = (Parser as BluCSSParser);
+
+            ImageLayer layer = new ImageLayer(bluParser.DebuggerMode ? null : bluParser.ActiveScreen.Content.Load<Texture2D>(valueMatch.Groups[1].Value.Replace('/', '\\')));
+            layer.Name = nameMatch.Value;
+            return layer; 
+        }
+    }
+}
