379de1425068d6fb37c85f0495e6ed49976aabb5
Hookkshot <stachkeirron@gmail.com>
2013-10-08 18:16:57 +1030

    Changed Filed directories
    Re worked directories to work with a vs2010 solution file

--- BluEngine/Properties/AssemblyInfo.cs
+++ BluEngine/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Blurift Entertainment")]
-[assembly: AssemblyProduct("BluScreenManager")]
+[assembly: AssemblyProduct("BluEngine")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
